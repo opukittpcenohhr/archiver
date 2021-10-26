0882967609d99256e98481047118df6f6328a9aa
Umnov Dmitrii <umnovdimitry@icloud.com>
2021-10-26 22:52:36 +0300

    Delete archiver directory

--- archiver/node_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "TrieNode.h"
-
-#include <iostream>
-#include <queue>
-
-int main() {
-    std::priority_queue<TrieNode*, std::vector<TrieNode*>, TrieNodeCmp> symbols;
-}
--- archiver/trietest.cpp
+++
@@ -1,5 +0,0 @@
-#include "Trie.h"
-
-int main() {
-    Trie trie;
-}
